79d6355f9922da1e54c397ff34f6a7a47fef6179
Vivek Rawat <vivekrawatyt01@gamil.com>
2026-02-07 01:54:37 +0530

    Final version

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const path = require('path');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -22,13 +23,22 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-// Routes (Placeholder)
-app.get('/', (req, res) => {
-    res.json({ message: 'API is running...' });
-});
-
+// Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, '../', 'client', 'dist', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.json({ message: 'API is running...' });
+    });
+}
 app.use(require('./middleware/errorMiddleware').errorHandler);
 
 const PORT = process.env.PORT || 5000;
